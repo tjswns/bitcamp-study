1e4714cd1f8869b18ffb3abb7f048ce139f35046
tjswns <tjswns99@naver.com>
2023-05-26 17:57:29 +0900

    .

---
+++ test/test11.java
@@ -0,0 +1,15 @@
+package test;
+
+public class test11 {
+
+  public static void main(String[] args) {
+
+    System.out.println(3.14);
+
+
+    System.out.println(0.0314e2);
+    System.out.println(0.314e1);
+    System.out.println(31.4e-1);
+    System.out.println(314e-2);
+  }
+}
--- test/test9.java
+++ test/test9.java
@@ -3,7 +3,7 @@ package test;
 public class test9 {
 
   public static void main(String[] args) {
-    System.out.println(5-3);
+    System.out.println(5 - 3);
     System.out.println(5 + (-3));
   }
 }
