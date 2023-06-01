3e612426802b7a6b138a4b58f8b685417b20dd28
tjswns <tjswns99@naver.com>
2023-06-01 20:16:57 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/test.java
+++ myapp/app/src/main/java/bitcamp/myapp/test.java
@@ -3,15 +3,8 @@ package bitcamp.myapp;
 public class test {
 
   public static void main(String[] args) {
-    int[] no = new int[5];
+    int[] no = new int[5] { 100, 200, 300};
+    System.out.println(no.length);
 
-    int[] no2;
-    no2 = no;
-
-    
-    no2[2] = 31;
-
-    System.out.println(no[2]);
   }
-
 }
---
+++ test/test42.java
@@ -0,0 +1,11 @@
+package test;
+
+public class test42 {
+
+  public static void main(String[] args) {
+    int a = 5;
+    int r = ++a;
+
+    System.out.printf("%d, %d\n", a, r);
+  }
+}
