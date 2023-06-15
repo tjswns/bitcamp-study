389c2d2e69942a1a1fe83f3a9b5a3a30db05a3ca
tjswns <tjswns99@naver.com>
2023-06-15 16:17:45 +0900

    .

--- test/app/src/main/java/bitcamp/myapp/handler/BoardHandler.java
+++ test/app/src/main/java/bitcamp/myapp/handler/BoardHandler.java
@@ -3,113 +3,115 @@ package bitcamp.myapp.handler;
 import bitcamp.myapp.vo.Board;
 import bitcamp.util.Prompt;
 
-public class BoardHandler {
+public class BoardHandler implements Handler {
 
-  // 인스턴스에 상관없이 공통으로 사용하는 필드라면 스태틱 필드로 선언한다.
-  private static final int MAX_SIZE = 100;
 
-  // 인스턴스 마다 별개로 관리해야 할 데이터라면 논스태틱 필드(인스턴스 필드)로 선언한다.
+  private BoardList list = new BoardList();
   private Prompt prompt;
-  private Board[] boards = new Board[MAX_SIZE];
-  private int length = 0;
 
-  public BoardHandler(Prompt prompt) {
+  private String title;
+
+  public BoardHandler(Prompt prompt, String title) {
     this.prompt = prompt;
+    this.title = title;
   }
 
-  // 인스턴스 멤버(필드나 메서드)를 사용하는 경우 인스턴스 메서드로 정의해야 한다.
-  public void inputBoard() {
-    if (!this.available()) {
-      System.out.println("더이상 입력할 수 없습니다!");
-      return;
-    }
+  public void execute() {
+    printMenu();
 
+    while (true) {
+      String menuNo = prompt.inputString("%s> ", this.title);
+      if (menuNo.equals("0")) {
+        return;
+      } else if (menuNo.equals("menu")) {
+        printMenu();
+      } else if (menuNo.equals("1")) {
+        this.inputBoard();
+      } else if (menuNo.equals("2")) {
+        this.printBoards();
+      } else if (menuNo.equals("3")) {
+        this.viewBoard();
+      } else if (menuNo.equals("4")) {
+        this.updateBoard();
+      } else if (menuNo.equals("5")) {
+        this.deleteBoard();
+      } else {
+        System.out.println("메뉴 번호가 옳지 않습니다!");
+      }
+    }
+  }
+
+  private static void printMenu() {
+    System.out.println("1. 등록");
+    System.out.println("2. 목록");
+    System.out.println("3. 조회");
+    System.out.println("4. 변경");
+    System.out.println("5. 삭제");
+    System.out.println("0. 메인");
+  }
+
+  private void inputBoard() {
     Board board = new Board();
     board.setTitle(this.prompt.inputString("제목? "));
     board.setContent(this.prompt.inputString("내용? "));
     board.setWriter(this.prompt.inputString("작성자? "));
     board.setPassword(this.prompt.inputString("암호? "));
 
-    this.boards[this.length++] = board;
+    if (!this.list.add(board)) {
+      System.out.println("더이상 입력할 수 없습니다!");
+      return;
+    }
   }
 
-  public void printBoards() {
+  private void printBoards() {
     System.out.println("---------------------------------------");
     System.out.println("번호, 제목, 작성자, 조회수, 등록일");
     System.out.println("---------------------------------------");
 
-    for (int i = 0; i < this.length; i++) {
-      Board board = this.boards[i];
-
-      System.out.printf("%d, %s, %s, %d, %tY-%5$tm-%5$td\n",
-          board.getNo(),
-          board.getTitle(),
-          board.getWriter(),
-          board.getViewCount(),
-          board.getCreatedDate());
+    Board[] arr = this.list.list();
+    for (Board board : arr) {
+      System.out.printf("%d, %s, %s, %d, %tY-%5$tm-%5$td\n", board.getNo(), board.getTitle(),
+          board.getWriter(), board.getViewCount(), board.getCreatedDate());
     }
   }
 
-  public void viewBoard() {
-    String boardNo = this.prompt.inputString("번호? ");
-    for (int i = 0; i < this.length; i++) {
-      Board board = this.boards[i];
-      if (board.getNo() == Integer.parseInt(boardNo)) {
-        System.out.printf("제목: %s\n", board.getTitle());
-        System.out.printf("내용: %s\n", board.getContent());
-        System.out.printf("작성자: %s\n", board.getWriter());
-        System.out.printf("조회수: %s\n", board.getViewCount());
-        System.out.printf("등록일: %tY-%1$tm-%1$td\n", board.getCreatedDate());
-        board.setViewCount(board.getViewCount() + 1);
-        return;
-      }
-    }
-    System.out.println("해당 번호의 게시글이 없습니다!");
-  }
+  private void viewBoard() {
+    int boardNo = this.prompt.inputInt("번호? ");
 
-  public void updateBoard() {
-    String boardNo = this.prompt.inputString("번호? ");
-    for (int i = 0; i < this.length; i++) {
-      Board board = this.boards[i];
-      if (board.getNo() == Integer.parseInt(boardNo)) {
-        if (!this.prompt.inputString("암호? ").equals(board.getPassword())) {
-          System.out.println("암호가 일치하지 않습니다!");
-          return;
-        }
-        board.setTitle(this.prompt.inputString("제목(%s)? ", board.getTitle()));
-        board.setContent(this.prompt.inputString("내용(%s)? ", board.getContent()));
-        return;
-      }
-    }
-    System.out.println("해당 번호의 게시글이 없습니다!");
-  }
-
-
-  public void deleteBoard() {
-    int deletedIndex = indexOf(this.prompt.inputInt("번호? "));
-    if (deletedIndex == -1) {
+    Board board = this.list.get(boardNo);
+    if (board == null) {
       System.out.println("해당 번호의 게시글이 없습니다!");
       return;
     }
 
-    for (int i = deletedIndex; i < this.length - 1; i++) {
-      this.boards[i] = this.boards[i + 1];
-    }
-
-    this.boards[--this.length] = null;
+    System.out.printf("제목: %s\n", board.getTitle());
+    System.out.printf("내용: %s\n", board.getContent());
+    System.out.printf("작성자: %s\n", board.getWriter());
+    System.out.printf("조회수: %s\n", board.getViewCount());
+    System.out.printf("등록일: %tY-%1$tm-%1$td\n", board.getCreatedDate());
   }
 
-  private int indexOf(int boardNo) {
-    for (int i = 0; i < this.length; i++) {
-      Board board = this.boards[i];
-      if (board.getNo() == boardNo) {
-        return i;
-      }
+  private void updateBoard() {
+    int boardNo = this.prompt.inputInt("번호? ");
+
+    Board board = this.list.get(boardNo);
+    if (board == null) {
+      System.out.println("해당 번호의 게시글이 없습니다!");
+      return;
     }
-    return -1;
+
+    if (!this.prompt.inputString("암호? ").equals(board.getPassword())) {
+      System.out.println("암호가 일치하지 않습니다!");
+      return;
+    }
+
+    board.setTitle(this.prompt.inputString("제목(%s)? ", board.getTitle()));
+    board.setContent(this.prompt.inputString("내용(%s)? ", board.getContent()));
   }
 
-  private boolean available() {
-    return this.length < MAX_SIZE;
+  private void deleteBoard() {
+    if (!this.list.delete(this.prompt.inputInt("번호? "))) {
+      System.out.println("해당 번호의 게시글이 없습니다!");
+    }
   }
 }
--- test/app/src/main/java/bitcamp/myapp/handler/MemberHandler.java
+++ test/app/src/main/java/bitcamp/myapp/handler/MemberHandler.java
@@ -3,96 +3,118 @@ package bitcamp.myapp.handler;
 import bitcamp.myapp.vo.Member;
 import bitcamp.util.Prompt;
 
-public class MemberHandler {
+public class MemberHandler implements Handler {
 
-  private static final int MAX_SIZE = 100;
-  // variable initializer (변수초기화 문장) => static 블록으로 이동
-  // 단 final 변수는 static 블록에서 값을 할당하지 않고 그냥 상수로 취급.
 
+  private MemberList list = new MemberList();
   private Prompt prompt;
 
-  private Member[] members = new Member[MAX_SIZE];
-  // variable initializer (변수초기화 문장) => 생성자로 이동
 
-  private int length;
+  private String title;
 
-  // 필요한 값을 외부에서 받고 싶으면 파라미터(Prompt prompt)를 선언하시오!
-  public MemberHandler(Prompt prompt) {
+  public MemberHandler(Prompt prompt, String title) {
     this.prompt = prompt;
-
-    // 생성자 : 인스턴스를 사용할 수 있도록 유효한 값으로 초기화 시키는 일을 수행
-    // members = new Member[MAX_SIZE]; => 이렇게도 사용 가능
+    this.title = title;
   }
 
-  public void inputMember() {
-    if (!this.available()) {
-      System.out.println("더이상 입력할 수 없습니다!");
-      return;
-    }
+  // Handler 인터페이스에 선언된 대로 정의했다. 메서드를 정의했다.
+  // => "Handler 인터페이스를 구현했다" 라고 표현한다.
+  public void execute() {
+    printMenu();
 
+    while (true) {
+      String menuNo = prompt.inputString("%s> ", this.title);
+      if (menuNo.equals("0")) {
+        return;
+      } else if (menuNo.equals("menu")) {
+        printMenu();
+      } else if (menuNo.equals("1")) {
+        this.inputMember();
+      } else if (menuNo.equals("2")) {
+        this.printMembers();
+      } else if (menuNo.equals("3")) {
+        this.viewMember();
+      } else if (menuNo.equals("4")) {
+        this.updateMember();
+      } else if (menuNo.equals("5")) {
+        this.deleteMember();
+      } else {
+        System.out.println("메뉴 번호가 옳지 않습니다!");
+      }
+    }
+  }
+
+  private static void printMenu() {
+    System.out.println("1. 등록");
+    System.out.println("2. 목록");
+    System.out.println("3. 조회");
+    System.out.println("4. 변경");
+    System.out.println("5. 삭제");
+    System.out.println("0. 메인");
+  }
+
+  private void inputMember() {
     Member m = new Member();
     m.setName(this.prompt.inputString("이름? "));
     m.setEmail(this.prompt.inputString("이메일? "));
     m.setPassword(this.prompt.inputString("암호? "));
     m.setGender(inputGender((char) 0));
 
-    // 위에서 만든 Member 인스턴스의 주소를 잃어버리지 않게
-    // 레퍼런스 배열에 담는다.
-
-    this.members[this.length++] = m;
+    if (!this.list.add(m)) {
+      System.out.println("입력 실패입니다.!");
+    }
   }
 
-  public void printMembers() {
+  private void printMembers() {
     System.out.println("---------------------------------------");
     System.out.println("번호, 이름, 이메일, 성별");
     System.out.println("---------------------------------------");
 
-    for (int i = 0; i < this.length; i++) {
-      Member m = this.members[i];
+    Member[] arr = this.list.list();
+    for (Member m : arr) {
       System.out.printf("%d, %s, %s, %s\n", m.getNo(), m.getName(), m.getEmail(),
           toGenderString(m.getGender()));
     }
   }
 
-  public void viewMember() {
-    String memberNo = this.prompt.inputString("번호? ");
-    for (int i = 0; i < this.length; i++) {
-      Member m = this.members[i];
-      if (m.getNo() == Integer.parseInt(memberNo)) {
-        System.out.printf("이름: %s\n", m.getName());
-        System.out.printf("이메일: %s\n", m.getEmail());
-        System.out.printf("성별: %s\n", toGenderString(m.getGender()));
-        return;
-      }
+  private void viewMember() {
+    int memberNo = this.prompt.inputInt("번호? ");
+    Member m = this.list.get(memberNo);
+    if (m == null) {
+      System.out.println("해당 번호의 회원이 없습니다!");
+      return;
     }
-    System.out.println("해당 번호의 회원이 없습니다!");
+
+    System.out.printf("이름: %s\n", m.getName());
+    System.out.printf("이메일: %s\n", m.getEmail());
+    System.out.printf("성별: %s\n", toGenderString(m.getGender()));
   }
 
 
   // 메소가 instance를 쓰지 않으면 static으로 생성!!
 
-  public static String toGenderString(char gender) {
+  private static String toGenderString(char gender) {
     return gender == 'M' ? "남성" : "여성";
   }
 
-  public void updateMember() {
-    String memberNo = this.prompt.inputString("번호? ");
-    for (int i = 0; i < this.length; i++) {
-      Member m = this.members[i];
-      if (m.getNo() == Integer.parseInt(memberNo)) {
+  private void updateMember() {
+    int memberNo = this.prompt.inputInt("번호? ");
 
-        m.setName(this.prompt.inputString("이름(%s)? ", m.getName()));
-
-        m.setEmail(this.prompt.inputString("이메일(%s)? ", m.getEmail()));
-
-        m.setPassword(this.prompt.inputString("새암호? "));
-
-        m.setGender(inputGender(m.getGender()));
-        return;
-      }
+    Member m = (this.list.get(memberNo));
+    if (m == null) {
+      System.out.println("해당 번호의 회원이 없습니다!");
+      return;
     }
-    System.out.println("해당 번호의 회원이 없습니다!");
+    m.setName(this.prompt.inputString("이름(%s)? ", m.getName()));
+
+    m.setEmail(this.prompt.inputString("이메일(%s)? ", m.getEmail()));
+
+    m.setPassword(this.prompt.inputString("새암호? "));
+
+    m.setGender(inputGender(m.getGender()));
   }
+
+
 
   private char inputGender(char gender) {
     String label;
@@ -115,33 +137,12 @@ public class MemberHandler {
     }
   }
 
-  public void deleteMember() {
+  private void deleteMember() {
     int memberNo = this.prompt.inputInt("번호? ");
 
-    int deletedIndex = indexOf(memberNo);
-    if (deletedIndex == -1) {
+
+    if (!this.list.delete(memberNo)) {
       System.out.println("해당 번호의 회원이 없습니다!");
-      return;
     }
-
-    for (int i = deletedIndex; i < this.length - 1; i++) {
-      this.members[i] = this.members[i + 1];
-    }
-
-    this.members[--length] = null;
-  }
-
-  private int indexOf(int memberNo) {
-    for (int i = 0; i < this.length; i++) {
-      Member m = this.members[i];
-      if (m.getNo() == memberNo) {
-        return i;
-      }
-    }
-    return -1;
-  }
-
-  private boolean available() {
-    return this.length < MAX_SIZE;
   }
 }
