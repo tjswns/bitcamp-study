57ddd80de0d60d6581725bf3cd29b5c321423d7f
tjswns <tjswns99@naver.com>
2023-06-28 16:41:56 +0900

    .

--- test/app/src/main/java/bitcamp/test/Test.java
+++ test/app/src/main/java/bitcamp/test/Test.java
@@ -3,13 +3,16 @@ package bitcamp.test;
 public class Test {
 
   public static void main(String[] args) {
-    Object c = new Calculator2();
 
-    // Calculator2 c2 = (Calculator2) c;
-
-    System.out.println(((Calculator2) c).minus(100, 200));
-    System.out.println(((Calculator) c).plus(100, 200));
-
+    System.out.println(factorial(5));
   }
 
+
+
+  static int factorial(int value) {
+    if (value == 1) {
+      return 1;
+    }
+    return factorial(value - 1) * value;
+  }
 }
