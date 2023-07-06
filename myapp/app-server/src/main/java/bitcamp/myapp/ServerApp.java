ff7f3847de29e7c8e00e726a626efeff2b638fd4
tjswns <tjswns99@naver.com>
2023-07-06 18:00:06 +0900

    Day06 project

--- myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
@@ -9,6 +9,7 @@ import bitcamp.myapp.dao.BoardListDao;
 import bitcamp.myapp.dao.MemberDao;
 import bitcamp.myapp.dao.MemberListDao;
 import bitcamp.myapp.vo.Board;
+import bitcamp.myapp.vo.Member;
 import bitcamp.net.RequestEntity;
 import bitcamp.net.ResponseEntity;
 
@@ -34,6 +35,7 @@ public class ServerApp {
       System.out.println("실행 예) java ... bitcamp.myapp.ServerApp 포트번호");
       return;
     }
+
     ServerApp app = new ServerApp(Integer.parseInt(args[0]));
     app.execute();
     app.close();
@@ -55,12 +57,11 @@ public class ServerApp {
       String command = request.getCommand();
       System.out.println(command);
 
-      // 응답을 위한 HashMap 객체 준비
-      ResponseEntity response = new ResponseEntity();
-
       if (command.equals("quit")) {
         break;
       }
+
+      ResponseEntity response = new ResponseEntity();
 
       switch (command) {
         case "board/list":
@@ -83,7 +84,53 @@ public class ServerApp {
           response.status(ResponseEntity.SUCCESS).result(value);
           break;
         case "board/delete":
+          value = boardDao.delete(request.getObject(Integer.class));
+          response.status(ResponseEntity.SUCCESS).result(value);
+          break;
+        case "member/list":
+          response.status(ResponseEntity.SUCCESS).result(memberDao.list());
+          break;
+        case "member/insert":
+          memberDao.insert(request.getObject(Member.class));
+          response.status(ResponseEntity.SUCCESS);
+          break;
+        case "member/findBy":
+          Member member = memberDao.findBy(request.getObject(Integer.class));
+          if (member == null) {
+            response.status(ResponseEntity.FAILURE).result("해당 번호의 회원이 없습니다!");
+          } else {
+            response.status(ResponseEntity.SUCCESS).result(member);
+          }
+          break;
+        case "member/update":
+          value = memberDao.update(request.getObject(Member.class));
+          response.status(ResponseEntity.SUCCESS).result(value);
+          break;
+        case "member/delete":
+          value = memberDao.delete(request.getObject(Integer.class));
+          response.status(ResponseEntity.SUCCESS).result(value);
+          break;
+        case "reading/list":
+          response.status(ResponseEntity.SUCCESS).result(boardDao.list());
+          break;
+        case "reading/insert":
+          boardDao.insert(request.getObject(Board.class));
+          response.status(ResponseEntity.SUCCESS);
+          break;
+        case "reading/findBy":
+          board = boardDao.findBy(request.getObject(Integer.class));
+          if (board == null) {
+            response.status(ResponseEntity.FAILURE).result("해당 번호의 게시글이 없습니다!");
+          } else {
+            response.status(ResponseEntity.SUCCESS).result(board);
+          }
+          break;
+        case "reading/update":
           value = boardDao.update(request.getObject(Board.class));
+          response.status(ResponseEntity.SUCCESS).result(value);
+          break;
+        case "reading/delete":
+          value = boardDao.delete(request.getObject(Integer.class));
           response.status(ResponseEntity.SUCCESS).result(value);
           break;
         default:
@@ -98,4 +145,5 @@ public class ServerApp {
     socket.close();
   }
 }
+
 
