947c1bb768212f7374d31ac2030ae501ea0f3b1d
tjswns <tjswns99@naver.com>
2023-07-13 09:33:12 +0900

    Day13 (22 Menuprompt complete) project

--- myapp/app-project/src/main/java/bitcamp/util/MenuPrompt.java
+++ myapp/app-project/src/main/java/bitcamp/util/MenuPrompt.java
@@ -50,5 +50,38 @@ public class MenuPrompt extends Prompt {
         break;
       }
     }
+    // 사용자가 입력한 명령어를 history에 보관
+    if (this.commandHistory.size() == 10) {
+      // 명령어 목록은 최대 10개만 유지한다.
+      // 10개를 초과할 경우 맨앞의 기록을 삭제한다.
+      this.commandHistory.poll();
+    }
+    String menuItem = findMenuItem(command);
+    if (menuItem != null) {
+      this.commandHistory.offer(titleBuilder.toString() + ": " + menuItem);
+    } else {
+      this.commandHistory.offer(command);
+    }
+    return command;
+  }
+
+  private String findMenuItem(String command) {
+    String menuTitle = null;
+
+    // command에 해당하는 메뉴가 있다면 그 메뉴 이름을 리턴하고
+    // 없다면 null을 리턴한다.
+
+    // 1) 현재 메뉴를 알아낸다. 메뉴 스택에서 맨 마지막에 입력한 메뉴를 조회한다.
+    String menu = (String) this.menus.peek();
+
+    // 2) 꺼낸 메뉴에서 해당 번호의 메뉴를 찾는다.
+    String[] menuItems = menu.split("\n");
+    for (String menuItem : menuItems) {
+      if (menuItem.startsWith(command)) {
+        return menuItem;
+      }
+    }
+
+    return menuTitle;
   }
 }
