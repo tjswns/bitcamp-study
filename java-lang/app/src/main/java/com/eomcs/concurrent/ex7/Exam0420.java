c3eb7c5bcfb7a3ac12407c0b7fb8971d7cacd968
tjswns <tjswns99@naver.com>
2023-07-17 17:08:53 +0900

    Day13 (22 complete) project

--- java-lang/app/src/main/java/com/eomcs/concurrent/ex7/Exam0420.java
+++ java-lang/app/src/main/java/com/eomcs/concurrent/ex7/Exam0420.java
@@ -19,11 +19,11 @@ public class Exam0420 {
       try {
         System.out.printf("[%d] %s 스레드 실행 중...\n", this.millisec, Thread.currentThread().getName());
 
-        Thread.sleep(millisec);
-        // for (long i = 0; i < 10000_0000; i++) {
-        // double r = Math.tan(3456.77889) / Math.random();
-        //
-        // }
+        // Thread.sleep(millisec);
+        for (long i = 0; i < 10000_0000; i++) {
+          double r = Math.tan(3456.77889) / Math.random();
+          //
+        }
 
         System.out.printf("[%d] %s 스레드 종료!\n", this.millisec, Thread.currentThread().getName());
 
@@ -46,9 +46,11 @@ public class Exam0420 {
 
     // 가능한 현재 수행 중인 작업들을 모두 멈추도록 지시한다.
     // => shutdown()과 차이점:
-    // - 만약 작업을 실행 중인 스레드가 잠시 Not Runnable 상태에 놓인다면,
+    // - 만약 Running 상태의 스레드가 잠시 Not Runnable 상태(sleep()/wait())에 놓인다면,
     // 바로 스레드를 멈출 기회라고 보고 스레드 종료를 시도할 것이다.
+    // -즉 실행중인 작업만 완료시키고, 대기 중인 작업은 취소하는 효과가 있다.
     // - 이런 이유로 shutdown() 보다는 스레드 종료를 좀 더 강제하는 효과가 있다.
+    // => Running 상태일 때는 작업이 끝나거나 Not Runnable
     // => 작업 중인 스레드가 Not Runnable 상태가 아니라면 작업이 끝날 때까지 기다린다.
     // => 다만 작업을 마치고 대기 중인 스레드는 즉시 취소한다.
     // => 그리고 취소한 대기 작업 목록을 리턴해준다.
@@ -67,7 +69,7 @@ public class Exam0420 {
 
     // shutdown() vs shutdownNow();
     // - shutdown()
-    // 진행 중인 작업을 완료하고 대기 중인 작업도 완료한 다음 종료.
+    // 진행 중인 작업을 완료하고 대기 중인 작업은 취소하고 그 목록을 리턴한다.
     // - shutdownNow()
     // 진행 중인 작업을 즉시 종료하고, 대기 중인 작업 목록은 리턴한다.
     System.out.println("main() 종료!");
---
+++ myapp/app-project22/src/main/java/bitcamp/myapp/App.java
@@ -0,0 +1,60 @@
+package bitcamp.myapp;
+
+import bitcamp.myapp.handler.AccHandler;
+import bitcamp.myapp.handler.Handler;
+import bitcamp.myapp.handler.MemberHandler;
+import bitcamp.myapp.handler.StylingHandler;
+import bitcamp.util.ArrayList;
+import bitcamp.util.MenuPrompt;
+
+public class App {
+
+  public static void main(String[] args) {
+
+    // 기본 생성자를 이용해 Prompt 인스턴스를 준비한다.
+    // => 기본 생성자는 Scanner를 키보드와 연결한다.
+    MenuPrompt prompt = new MenuPrompt();
+    prompt.appendBreadcrumb("메인", getMenu());
+    // 모든 핸들러는 Handler 규칙에 따라 정의되었기 때문에
+    // Handler 레퍼런스에 그 주소를 담을 수 있다.
+    Handler memberHandler = new MemberHandler(prompt, "회원", new ArrayList());
+    Handler stylingHandler = new StylingHandler(prompt, "스타일", new ArrayList());
+    Handler accHandler = new AccHandler(prompt, "악세서리", new ArrayList());
+    printTitle();
+
+    prompt.printMenu();
+
+    loop: while (true) {
+      String menuNo = prompt.inputMenu();
+      switch (menuNo) {
+        case "0":
+          break loop;
+        case "1":
+          memberHandler.execute();
+          break;
+        case "2":
+          stylingHandler.execute();
+          break;
+        case "3":
+          accHandler.execute();
+          break;
+      }
+    }
+
+    prompt.close();
+  }
+
+  static String getMenu() {
+    StringBuilder menu = new StringBuilder();
+    menu.append("1. 회원\n");
+    menu.append("2. 스타일\n");
+    menu.append("3. 악세서리\n");
+    menu.append("0. 종료\n");
+    return menu.toString();
+  }
+
+  static void printTitle() {
+    System.out.println("회원별 옷 사이즈 관리 시스템");
+    System.out.println("----------------------------------");
+  }
+}
