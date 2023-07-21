8cb85151e7dd57d70b1da4ac5c02d157229635d0
tjswns <tjswns99@naver.com>
2023-07-21 19:12:20 +0900

    Day21 (23 Styling complete) project

--- myapp/app-project/src/main/java/bitcamp/myapp/handler/MemberHandler.java
+++ myapp/app-project/src/main/java/bitcamp/myapp/handler/MemberHandler.java
@@ -1,19 +1,16 @@
 package bitcamp.myapp.handler;
 
 import bitcamp.myapp.vo.Member;
-import bitcamp.util.List;
 import bitcamp.util.BreadcrumbPrompt;
 
 public class MemberHandler implements Handler {
 
-  private List list;
   private BreadcrumbPrompt prompt;
   private String title;
 
-  public MemberHandler(BreadcrumbPrompt prompt, String title, List list) {
+  public MemberHandler(BreadcrumbPrompt prompt, String title) {
     this.prompt = prompt;
     this.title = title;
-    this.list = list;
   }
 
   public void execute() {
@@ -97,9 +94,6 @@ public class MemberHandler implements Handler {
 
   }
 
-  public String toGenderString(char gender) {
-    return gender == 'M' ? "남성" : "여성";
-  }
 
   public void updateMember() {
     int memberNo = this.prompt.inputInt("번호? ");
@@ -116,155 +110,7 @@ public class MemberHandler implements Handler {
     inputShoes(m);
   }
 
-  private char inputGender(char gender) {
-    String label;
-    if (gender == 0) {
-      label = "성별?\n";
-    } else {
-      label = String.format("성별(%s)?\n", toGenderString(gender));
-    }
-    while (true) {
-      String menuNo = this.prompt.inputString(label + "  1. 남자\n" + "  2. 여자\n" + "> ");
 
-      switch (menuNo) {
-        case "1":
-          return Member.MALE;
-        case "2":
-          return Member.FEMALE;
-        default:
-          System.out.println("무효한 번호입니다.");
-      }
-    }
-  }
-
-  private void inputTop(Member m) {
-    if (m.getGender() == Member.MALE) {
-      loop: while (true) {
-        String menuNo =
-            this.prompt.inputString("상의:\n" + "  1. M\n" + "  2. L\n" + "  3. XL\n" + "> ");
-
-        switch (menuNo) {
-          case "1":
-            m.setTop("M");
-            break loop;
-          case "2":
-            m.setTop("L");
-            break loop;
-          case "3":
-            m.setTop("XL");
-            break loop;
-          default:
-            System.out.println("무효한 번호입니다.");
-        }
-      }
-    } else if (m.getGender() == Member.FEMALE) {
-
-      loop: while (true) {
-        String menuNo =
-            this.prompt.inputString("상의:\n" + "  1. XS\n" + "  2. S\n" + "  3. M\n" + "> ");
-
-        switch (menuNo) {
-          case "1":
-            m.setTop("XS");
-            break loop;
-          case "2":
-            m.setTop("S");
-            break loop;
-          case "3":
-            m.setTop("M");
-            break loop;
-          default:
-            System.out.println("무효한 번호입니다.");
-        }
-      }
-    }
-  }
-
-  private void inputPants(Member m) {
-    if (m.getGender() == Member.MALE) {
-      loop: while (true) {
-        String menuNo =
-            this.prompt.inputString("바지:\n" + "  1. 28\n" + "  2. 30\n" + "  3. 32\n" + "> ");
-
-        switch (menuNo) {
-          case "1":
-            m.setPants("28");
-            break loop;
-          case "2":
-            m.setPants("30");
-            break loop;
-          case "3":
-            m.setPants("32");
-            break loop;
-          default:
-            System.out.println("무효한 번호입니다.");
-        }
-      }
-    } else if (m.getGender() == Member.FEMALE) {
-
-      loop: while (true) {
-        String menuNo =
-            this.prompt.inputString("바지:\n" + "  1. 24\n" + "  2. 26\n" + "  3. 28\n" + "> ");
-
-        switch (menuNo) {
-          case "1":
-            m.setPants("24");
-            break loop;
-          case "2":
-            m.setPants("26");
-            break loop;
-          case "3":
-            m.setPants("28");
-            break loop;
-          default:
-            System.out.println("무효한 번호입니다.");
-        }
-      }
-    }
-  }
-
-  private void inputShoes(Member m) {
-    if (m.getGender() == Member.MALE) {
-      loop: while (true) {
-        String menuNo =
-            this.prompt.inputString("신발:\n" + "  1. 260\n" + "  2. 265\n" + "  3. 270\n" + "> ");
-
-        switch (menuNo) {
-          case "1":
-            m.setShoes("260");
-            break loop;
-          case "2":
-            m.setShoes("265");
-            break loop;
-          case "3":
-            m.setShoes("270");
-            break loop;
-          default:
-            System.out.println("무효한 번호입니다.");
-        }
-      }
-    } else if (m.getGender() == Member.FEMALE) {
-
-      loop: while (true) {
-        String menuNo =
-            this.prompt.inputString("신발:\n" + "  1. 240\n" + "  2. 245\n" + "  3. 250\n" + "> ");
-
-        switch (menuNo) {
-          case "1":
-            m.setShoes("240");
-            break loop;
-          case "2":
-            m.setShoes("245");
-            break loop;
-          case "3":
-            m.setShoes("250");
-            break loop;
-          default:
-            System.out.println("무효한 번호입니다.");
-        }
-      }
-    }
-  }
 
   public void deleteMember() {
     if (!this.list.remove(new Member(this.prompt.inputInt("번호? ")))) {
@@ -273,13 +119,5 @@ public class MemberHandler implements Handler {
 
   }
 
-  private Member findBy(int no) {
-    for (int i = 0; i < this.list.size(); i++) {
-      Member m = (Member) this.list.get(i);
-      if (m.getNo() == no) {
-        return m;
-      }
-    }
-    return null;
-  }
+
 }
