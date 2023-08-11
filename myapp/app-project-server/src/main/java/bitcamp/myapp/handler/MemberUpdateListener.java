02fb9bcc4eaa6a388194783677f682bf05983f9e
tjswns <tjswns99@naver.com>
2023-08-11 16:58:39 +0900

    Day11 (55 ing) project

--- myapp/app-project-server/src/main/java/bitcamp/myapp/handler/MemberUpdateListener.java
+++ myapp/app-project-server/src/main/java/bitcamp/myapp/handler/MemberUpdateListener.java
@@ -1,14 +1,16 @@
 package bitcamp.myapp.handler;
 
-import java.io.IOException;
+import java.io.PrintWriter;
 import org.apache.ibatis.session.SqlSessionFactory;
 import bitcamp.myapp.dao.MemberDao;
 import bitcamp.myapp.vo.Member;
-import bitcamp.util.BreadcrumbPrompt;
 import bitcamp.util.Component;
+import bitcamp.util.HttpServletRequest;
+import bitcamp.util.HttpServletResponse;
+import bitcamp.util.Servlet;
 
 @Component("/member/update")
-public class MemberUpdateListener implements MemberActionListener {
+public class MemberUpdateListener implements Servlet {
 
   MemberDao memberDao;
   SqlSessionFactory sqlSessionFactory;
@@ -19,29 +21,46 @@ public class MemberUpdateListener implements MemberActionListener {
   }
 
   @Override
-  public void service(BreadcrumbPrompt prompt) throws IOException {
-    int memberNo = prompt.inputInt("번호? ");
-    Member m = memberDao.findBy(memberNo);
-    if (m == null) {
-      prompt.println("해당 번호의 회원이 없습니다!");
-      return;
-    }
-    m.setName(prompt.inputString("이름(%s)?", m.getName()));
-    m.setEmail(prompt.inputString("이메일(%s)? ", m.getEmail()));
-    m.setPassword(prompt.inputString("새암호? "));
-    m.setAge(prompt.inputString("나이(%s)? ", m.getAge()));
-    m.setGender(MemberActionListener.inputGender(m.getGender(), prompt));
-    MemberActionListener.inputTop(m, prompt);
-    MemberActionListener.inputPants(m, prompt);
-    MemberActionListener.inputShoes(m, prompt);
+  public void service(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+    Member member = new Member();
+    member.setNo(Integer.parseInt(request.getParameter("no")));
+    member.setName(request.getParameter("name"));
+    member.setEmail(request.getParameter("email"));
+    member.setAge(request.getParameter("age"));
+    member.setPassword(request.getParameter("password"));
+    member.setGender(request.getParameter("gender").charAt(0));
+    member.setTop(request.getParameter("top"));
+    member.setPants(request.getParameter("pants"));
+    member.setShoes(request.getParameter("shoes"));
+
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<meta charset='UTF-8'>");
+    out.println("<meta http-equiv='refresh' content='1;url=/member/list'>");
+    out.println("<title>회원</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>회원 변경</h1>");
 
     try {
-      memberDao.update(m);
-      sqlSessionFactory.openSession(false).commit();
-
+      if (memberDao.update(member) == 0) {
+        out.println("<p>회원이 없습니다.</p>");
+      } else {
+        sqlSessionFactory.openSession(false).commit();
+        out.println("<p>변경했습니다!</p>");
+      }
     } catch (Exception e) {
       sqlSessionFactory.openSession(false).rollback();
-      throw new RuntimeException(e);
+      out.println("<p>변경 실패입니다!</p>");
+      e.printStackTrace();
     }
+
+    out.println("</body>");
+    out.println("</html>");
   }
+
 }
