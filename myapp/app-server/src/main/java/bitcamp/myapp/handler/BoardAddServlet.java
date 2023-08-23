d070801114518dfbdda098688acce01f25e264d2
tjswns <tjswns99@naver.com>
2023-08-23 19:47:03 +0900

    Day23 (55 ing) project

--- myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardAddServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardAddServlet.java
@@ -11,9 +11,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+import bitcamp.myapp.dao.BoardDao;
 import bitcamp.myapp.vo.AttachedFile;
 import bitcamp.myapp.vo.Board;
 import bitcamp.myapp.vo.Member;
+import bitcamp.util.NcpObjectStorageService;
+import org.apache.ibatis.session.SqlSessionFactory;
 
 @WebServlet("/board/add")
 @MultipartConfig(maxFileSize = 1024 * 1024 * 10)
@@ -31,6 +34,10 @@ public class BoardAddServlet extends HttpServlet {
       return;
     }
 
+    BoardDao boardDao = (BoardDao) this.getServletContext().getAttribute("boardDao");
+    SqlSessionFactory sqlSessionFactory = (SqlSessionFactory) this.getServletContext().getAttribute("sqlSessionFactory");
+    NcpObjectStorageService ncpObjectStorageService = (NcpObjectStorageService) this.getServletContext().getAttribute("ncpObjectStorageService");
+
     try {
       Board board = new Board();
       board.setWriter(loginUser);
@@ -41,7 +48,7 @@ public class BoardAddServlet extends HttpServlet {
       ArrayList<AttachedFile> attachedFiles = new ArrayList<>();
       for (Part part : request.getParts()) {
         if (part.getName().equals("files") && part.getSize() > 0) {
-          String uploadFileUrl = InitServlet.ncpObjectStorageService.uploadFile(
+          String uploadFileUrl = ncpObjectStorageService.uploadFile(
                   "bitcamp-nc7-bucket-17", "board/", part);
           AttachedFile attachedFile = new AttachedFile();
           attachedFile.setFilePath(uploadFileUrl);
@@ -50,23 +57,23 @@ public class BoardAddServlet extends HttpServlet {
       }
       board.setAttachedFiles(attachedFiles);
 
-
-      InitServlet.boardDao.insert(board);
+      boardDao.insert(board);
       if (attachedFiles.size() > 0) {
-        InitServlet.boardDao.insertFiles(board);
+        boardDao.insertFiles(board);
       }
 
-      InitServlet.sqlSessionFactory.openSession(false).commit();
-      response.sendRedirect("list?category=" + board.getCategory());
+      sqlSessionFactory.openSession(false).commit();
+      response.sendRedirect("list?category=" + request.getParameter("category"));
 
     } catch (Exception e) {
-      InitServlet.sqlSessionFactory.openSession(false).rollback();
+      sqlSessionFactory.openSession(false).rollback();
 
       // ErrorServlet 으로 포워딩 하기 전에 ErrorServlet이 사용할 데이터를
-      // ServletRequest보관소에 관련 정보를 저장해 둔다.
+      // ServletRequest 보관소에 저장한다.
       request.setAttribute("error", e);
       request.setAttribute("message", "게시글 등록 오류!");
       request.setAttribute("refresh", "2;url=list?category=" + request.getParameter("category"));
+
       request.getRequestDispatcher("/error").forward(request, response);
     }
   }
--- myapp/app-server/src/main/java/bitcamp/myapp/handler/MemberDetailServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/MemberDetailServlet.java
@@ -7,7 +7,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import bitcamp.myapp.dao.MemberDao;
 import bitcamp.myapp.vo.Member;
+import bitcamp.util.NcpObjectStorageService;
+import org.apache.ibatis.session.SqlSessionFactory;
 
 @WebServlet("/member/detail")
 public class MemberDetailServlet extends HttpServlet {
@@ -16,9 +20,11 @@ public class MemberDetailServlet extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+          throws ServletException, IOException {
 
-    Member member = InitServlet.memberDao.findBy(Integer.parseInt(request.getParameter("no")));
+    MemberDao memberDao = (MemberDao) this.getServletContext().getAttribute("memberDao");
+
+    Member member = memberDao.findBy(Integer.parseInt(request.getParameter("no")));
 
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
@@ -29,6 +35,9 @@ public class MemberDetailServlet extends HttpServlet {
     out.println("<title>회원</title>");
     out.println("</head>");
     out.println("<body>");
+
+    request.getRequestDispatcher("/header").include(request, response);
+
     out.println("<h1>회원</h1>");
 
     if (member == null) {
@@ -38,27 +47,27 @@ public class MemberDetailServlet extends HttpServlet {
       out.println("<form action='/member/update' method='post' enctype='multipart/form-data'>");
       out.println("<table border='1'>");
       out.printf("<tr><th style='width:120px;'>사진</th>"
-          + " <td style='width:300px;'>"
-          + (member.getPhoto() == null ? "<img style='height:80px' src='/images/avatar.png'>" :
-            "<a href='https://kr.object.ncloudstorage.com/bitcamp-nc7-bucket-17/member/%s'>"
-            + "<img src='http://mvsenqskbqzl19010704.cdn.ntruss.com/member/%1$s?type=f&w=60&h=80&faceopt=true&ttype=jpg'>"
-            + "</a>")
-          + " <input type='file' name='photo'>"
-          + "</td></tr>\n", member.getPhoto());
+              + " <td style='width:300px;'>"
+              + (member.getPhoto() == null ? "<img style='height:80px' src='/images/avatar.png'>" :
+              "<a href='https://kr.object.ncloudstorage.com/bitcamp-nc7-bucket-17/member/%s'>"
+                      + "<img src='http://mvsenqskbqzl19010704.cdn.ntruss.com/member/%1$s?type=f&w=60&h=80&faceopt=true&ttype=jpg'>"
+                      + "</a>")
+              + " <input type='file' name='photo'>"
+              + "</td></tr>\n", member.getPhoto());
       out.printf("<tr><th style='width:120px;'>번호</th>"
-          + " <td style='width:300px;'><input type='text' name='no' value='%d' readonly></td></tr>\n", member.getNo());
+              + " <td style='width:300px;'><input type='text' name='no' value='%d' readonly></td></tr>\n", member.getNo());
       out.printf("<tr><th>이름</th>"
-          + " <td><input type='text' name='name' value='%s'></td></tr>\n", member.getName());
+              + " <td><input type='text' name='name' value='%s'></td></tr>\n", member.getName());
       out.printf("<tr><th>이메일</th>"
-          + " <td><input type='email' name='email' value='%s'></td></tr>\n", member.getEmail());
+              + " <td><input type='email' name='email' value='%s'></td></tr>\n", member.getEmail());
       out.println("<tr><th>암호</th>"
-          + " <td><input type='password' name='password'></td></tr>");
+              + " <td><input type='password' name='password'></td></tr>");
       out.printf("<tr><th>성별</th>\n"
-          + " <td><select name='gender'>\n"
-          + " <option value='M' %s>남자</option>\n"
-          + " <option value='W' %s>여자</option></select></td></tr>\n",
-          (member.getGender() == 'M' ? "selected" : ""),
-          (member.getGender() == 'W' ? "selected" : ""));
+                      + " <td><select name='gender'>\n"
+                      + " <option value='M' %s>남자</option>\n"
+                      + " <option value='W' %s>여자</option></select></td></tr>\n",
+              (member.getGender() == 'M' ? "selected" : ""),
+              (member.getGender() == 'W' ? "selected" : ""));
       out.println("</table>");
 
       out.println("<div>");
@@ -69,6 +78,8 @@ public class MemberDetailServlet extends HttpServlet {
       out.println("</div>");
       out.println("</form>");
     }
+
+    request.getRequestDispatcher("/footer").include(request, response);
 
     out.println("</body>");
     out.println("</html>");
